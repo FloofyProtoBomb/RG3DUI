4ef3dfb5b6f5da31464144d7cf4c4b59f4f3f895
FloofyProtoBomb <96898033+FloofyProtoBomb@users.noreply.github.com>
2024-08-26 16:49:23 -0400

    Update cpu_thermal.sh

--- cpu_thermal.sh
+++ cpu_thermal.sh
@@ -1,7 +1,8 @@
-  for cpuseq in $(seq 1 60); do
+  for cpuseq in $(seq 1 60)
+  do
     v3=$(cat /sys/devices/virtual/thermal/thermal_zone$cpuseq/type 2>/dev/null)
     v4="soc"
-    if [[ "$v3" == "v4" ]]; then
+    if [[ "$v3" == "$v4" ]]; then
       cpu_temp_raw=$(cat /sys/devices/virtual/thermal/thermal_zone$cpuseq/temp 2>/dev/null)
       cpu_temp=$((cpu_temp_raw / 1000))
       break
